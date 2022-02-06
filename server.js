4dcae91ba36d87347e40ec4a24bfb8bba642d70e
Hiba Ali <hiba.mumin22@gmail.com>
2022-02-06 17:41:53 -0600

    created routes to serve other pages & finished GET requests

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const { animals } = require('./data/animals');
 
@@ -122,6 +125,16 @@ app.post('/api/animals', (req, res) => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+app.get('/zookeeper', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
